21b2d1de0f655c0017634788f59b5a3e2bd4756a
Enchan <enchant0130@gmail.com>
2024-04-25 22:29:10 +0900

    [Add] initial commit

---
+++ src/test/java/me/enchan/hello_world/AppTest.java
@@ -0,0 +1,18 @@
+package me.enchan.hello_world;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
